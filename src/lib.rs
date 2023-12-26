dd48d045ea486ec54e4ff6f7df3bdca000d49e8c
Aditya Dhara <dev@adityadhara.com>
2023-12-26 00:36:38 -0800

    Solved 19

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod factor;
 pub mod fib;
 pub mod primes;
+pub mod triangle;
 
 pub fn binom(n: u64, k: u64) -> u64 {
     ((k + 1)..=n).reduce(|acc, e| e * acc).unwrap_or(1)
---
+++ src/triangle.rs
@@ -0,0 +1,146 @@
+#[derive(Debug)]
+pub struct Triangle {
+    pub values: Vec<u32>,
+}
+
+#[derive(Debug)]
+pub struct TriangleElement<'a> {
+    pub value: u32,
+
+    row: usize,
+    col: usize,
+    triangle: &'a Triangle,
+}
+
+impl Triangle {
+    pub fn new(triangle_string: &str) -> Triangle {
+        let num_rows = triangle_string.trim().lines().count();
+
+        let values = triangle_string
+            .trim()
+            .lines()
+            .map(Triangle::split_line)
+            .flatten()
+            .collect::<Vec<u32>>();
+
+        assert_eq!((1..=num_rows).sum::<usize>(), values.len());
+
+        Triangle { values }
+    }
+
+    pub fn get_root<'a>(&'a self) -> TriangleElement<'a> {
+        TriangleElement {
+            value: *self.values.get(0).unwrap(),
+            row: 1,
+            col: 1,
+            triangle: &self,
+        }
+    }
+
+    pub fn get_item_at<'a>(&'a self, row: usize, col: usize) -> Option<TriangleElement<'a>> {
+        let pos: usize = (1..row).sum::<usize>() + (col - 1);
+
+        self.values
+            .get(pos)
+            .map(|val| *val)
+            .map(|value| TriangleElement {
+                value,
+                row,
+                col,
+                triangle: &self,
+            })
+    }
+
+    fn split_line(line: &str) -> Vec<u32> {
+        line.trim()
+            .split(" ")
+            .map(|item| item.parse().unwrap())
+            .collect()
+    }
+}
+
+impl<'a> TriangleElement<'a> {
+    pub fn get_left_child(&'a self) -> Option<TriangleElement<'a>> {
+        self.triangle.get_item_at(self.row + 1, self.col)
+    }
+
+    pub fn get_right_child(&'a self) -> Option<TriangleElement<'a>> {
+        self.triangle.get_item_at(self.row + 1, self.col + 1)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Triangle;
+
+    const MINI: &'static str = r#"
+    1
+    "#;
+
+    const SMALL: &'static str = r#"
+    1
+    2 3
+    "#;
+
+    const LARGE: &'static str = r#"
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    11 12 13 14 15
+    16 17 18 19 20 21
+    "#;
+
+    #[test]
+    fn creates_mini_triangle() {
+        let t = Triangle::new(MINI);
+        assert_eq!(t.get_root().value, 1);
+    }
+
+    #[test]
+    fn small_triangle_second_row() {
+        let t = Triangle::new(SMALL);
+        assert_eq!(t.get_root().value, 1);
+        assert_eq!(t.get_item_at(2, 1).unwrap().value, 2);
+        assert_eq!(t.get_item_at(2, 2).unwrap().value, 3);
+        assert!(t.get_item_at(3, 1).is_none());
+    }
+
+    #[test]
+    fn mini_triangle_node_branches() {
+        let t = Triangle::new(SMALL);
+        assert_eq!(t.get_root().value, 1);
+        assert_eq!(t.get_root().get_left_child().unwrap().value, 2);
+        assert_eq!(t.get_root().get_right_child().unwrap().value, 3);
+    }
+
+    #[test]
+    fn large_triangle_node_branches() {
+        let t = Triangle::new(LARGE);
+        assert_eq!(t.get_root().value, 1);
+        assert_eq!(
+            t.get_root()
+                .get_left_child()
+                .unwrap()
+                .get_left_child()
+                .unwrap()
+                .get_right_child()
+                .unwrap()
+                .value,
+            8
+        );
+        assert_eq!(
+            t.get_root()
+                .get_right_child()
+                .unwrap()
+                .get_right_child()
+                .unwrap()
+                .get_left_child()
+                .unwrap()
+                .get_right_child()
+                .unwrap()
+                .value,
+            14
+        );
+    }
+}
