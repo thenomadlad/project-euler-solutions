cccde1d00713a2b18ad12978eb8de8342e3ee0c2
Aditya Dhara <dev@adityadhara.com>
2024-08-30 20:15:31 -0700

    Solved 23 faster

--- examples/problem_023.rs
+++ examples/problem_023.rs
@@ -1,34 +1,54 @@
-use std::collections::HashSet;
-
+use itertools::Itertools;
 use project_euler_solutions::factor::divisor_sum;
 
 fn main() {
-    let mut abundant_numbers = HashSet::new();
+    let abundant_numbers = (1..=28123)
+        .map(|num| {
+            let sum_proper_divisors = divisor_sum(num) - num;
+            (sum_proper_divisors, num)
+        })
+        .filter(|(sum_proper_divisors, num)| sum_proper_divisors > num)
+        .map(|(_, num)| num as usize)
+        .collect_vec();
 
-    // find sufficient list of abundant numbers
-    for num in 1..=28123 {
-        let sum_proper_divisors = divisor_sum(num) - num;
+    // println!("first coupla abundants: {:?}", &abundant_numbers[0..15]);
 
-        if sum_proper_divisors > num {
-            abundant_numbers.insert(num);
+    // compute sums of abundants
+    let mut is_idx_abundant_sum = [false; 28124];
+    for (idx, lhs) in abundant_numbers.iter().enumerate() {
+        for rhs in &abundant_numbers[idx..] {
+            let val = lhs + rhs;
+            if val > 28123 {
+                break;
+            }
+
+            is_idx_abundant_sum[val] = true;
         }
     }
+
+    // uncomment to debug
+
+    // println!("is 24 a sum of abundants? {:?}", is_idx_abundant_sum[24]);
+    // println!("is 23 a sum of abundants? {:?}", is_idx_abundant_sum[23]);
+    // println!(
+    //     "is 28123 a sum of abundants? {:?}",
+    //     is_idx_abundant_sum[28123]
+    // );
+    // println!(
+    //     "largest non-sum-of-abundants? {:?}",
+    //     is_idx_abundant_sum
+    //         .iter()
+    //         .enumerate()
+    //         .filter(|(_, is_sum_of_abundants)| !*is_sum_of_abundants)
+    //         .map(|(num, _)| num)
+    //         .last()
+    // );  // 20161
 
     // find nums that are not sums of abundant nums
     println!(
         "{}",
-        (0..28123)
-            .filter(|num| !is_sum_abundant_nums(*num, &abundant_numbers))
-            .sum::<u64>()
+        (1..=28123)
+            .filter(|num| !is_idx_abundant_sum[*num])
+            .sum::<usize>()
     );
-}
-
-fn is_sum_abundant_nums(num: u64, abundant_numbers: &HashSet<u64>) -> bool {
-    for abundant_num in abundant_numbers.iter() {
-        if (*abundant_num < num) && abundant_numbers.contains(&(num - abundant_num)) {
-            return true;
-        }
-    }
-
-    false
 }
